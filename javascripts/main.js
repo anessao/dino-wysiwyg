f543fe7a7f2ab52eb54a49acb971b12ce216cb57
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-10 21:38:43 -0500

    final functionality and styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
-var dinoArray = [];
 function makeDom(array){
 	var myDomString = "";
+	myDomString += `<div class="row">`;
 	for (let x = 0; x < array.length; x++){
-		myDomString += `<div class="dinoCard">`;
+		myDomString += `<div class="dinoCard col-md-4">`;
 		myDomString += `<header><h1>${array[x].type}</h1></header>`;
 		myDomString += `<section>`;
 		myDomString += `<img src="${array[x].img}">`;
@@ -11,7 +11,12 @@ function makeDom(array){
 		myDomString += `<p>${array[x].type}</p>`;
 		myDomString += `<footer><h3>${array[x].info}</h3></footer>`;
 		myDomString += `</div>`;
+		if (x % 3 === 2) {
+			console.log("sup");
+			myDomString += `</div><div class="row">`;
+		}
 	}
+	console.log(myDomString);
 	$("#dinosaurs").html(myDomString);
 }
 
@@ -21,3 +26,30 @@ $.ajax("./db/dinosaurs.json").done(function(data){
 }).fail(function(error){
 	console.log("problem with yo data", error);
 });
+
+$("#dinosaurs").on("click", ".dinoCard", function(e){
+	$(".dinoCard").removeClass("dottedBorder");
+	$(this).addClass("dottedBorder");
+	$("#textbox").val("").focus();
+});
+$("#textbox").keyup(mirrorText);
+
+function mirrorText(e){
+	var selectedCard = $(".dottedBorder");
+	var bio = selectedCard.find(".bio");
+	var bioTyped = $("#textbox").val();
+	bio.html(bioTyped);
+	if (e.keyCode === 13){
+		$("#textbox").val("");
+	}
+}
+
+
+
+
+
+
+
+
+
+
