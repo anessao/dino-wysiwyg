edf0539e56bafb86d3db95c32e6bdf0708e5c8b2
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-10 19:54:13 -0500

    set up DOM elements with js and html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,23 @@
-var something = `<h3>My name is Anessa</h3>`;
-console.log(something);
+var dinoArray = [];
+function makeDom(array){
+	var myDomString = "";
+	for (let x = 0; x < array.length; x++){
+		myDomString += `<div class="dinoCard">`;
+		myDomString += `<header><h1>${array[x].type}</h1></header>`;
+		myDomString += `<section>`;
+		myDomString += `<img src="${array[x].img}">`;
+		myDomString += `<p class="bio">${array[x].bio}</p>`;
+		myDomString += `</section>`;
+		myDomString += `<p>${array[x].type}</p>`;
+		myDomString += `<footer><h3>${array[x].info}</h3></footer>`;
+		myDomString += `</div>`;
+	}
+	$("#dinosaurs").html(myDomString);
+}
+
+$.ajax("./db/dinosaurs.json").done(function(data){
+	dinoArray = data.dinosaurs;
+	makeDom(dinoArray);
+}).fail(function(error){
+	console.log("problem with yo data", error);
+});
